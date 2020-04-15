1931781beb2b06b623832dfbd6fb8e3c1c273a69
panos1962 <panos1962@gmail.com>
2020-04-15 09:07:13 +0300

    modifications

---
+++ www/lib/session.php
@@ -0,0 +1,47 @@
+<?php
+
+///////////////////////////////////////////////////////////////////////////////@
+//
+// @BEGIN
+//
+// @COPYRIGHT BEGIN
+// Copyright (C) 2020 Panos I. Papadopoulos <panos1962_AT_gmail_DOT_com>
+// @COPYRIGHT END
+//
+// @FILETYPE BEGIN
+// php
+// @FILETYPE END
+//
+// @FILE BEGIN
+// www/lib/session.php —— Πρόγραμμα ενημέρωσης των session cookies
+// @FILE END
+//
+// @DESCRIPTION BEGIN
+// Το παρόν καλείται προκειμένου να ενημερώσουμε στοιχεία των session cookies.
+// Πιο συγκεκριμένα, δέχεται μια λίστα από key, value pairs και ενημερώνει τα
+// αντίστοιχα στοιχεία της λίστας "_SESSION".
+// @DESCRIPTION END
+//
+// @HISTORY BEGIN
+// Created: 2020-04-15
+// @HISTORY END
+//
+// @END
+//
+///////////////////////////////////////////////////////////////////////////////@
+
+require_once("../../local/conf.php");
+require_once("pandora.php");
+
+pandora::session_init();
+
+if (!pandora::post_get("list"))
+exit(0);
+
+if (!is_array($_POST["list"]))
+exit(1);
+
+foreach ($_POST["list"] as $key => $val)
+$_SESSION[$key] = $Val;
+
+exit(0);
